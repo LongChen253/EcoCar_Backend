cf70593f001d68dd5d87f4c21f1589b73e074da4
LongChen253 <guapoguapo253@gmail.com>
2022-03-18 19:57:12 +0100

    login done

--- functions/index.js
+++ functions/index.js
@@ -12,14 +12,27 @@ exports.createUserDB = functions.https.onRequest((req, res) => {
     var qemail = req.query.email;
 
     usersRef.orderByKey().equalTo(qusername).once('value', (snapshot) => {
-        if (snapshot.exists()) res.json({result: false, ErrorMessage: "UserAlreadyExist!"});
+        if (snapshot.exists()) res.json({ result: false, ErrorMessage: "UserAlreadyExist!" });
         else {
             usersRef.child(qusername).set({
                 password: qpassword,
                 email: qemail
             });
-            res.json({result: true});
+            res.json({ result: true });
         }
 
+    })
+});
+
+exports.loginDB = functions.https.onRequest((req, res) => {
+    var qusername = req.query.username;
+    var qpassword = req.query.password;
+
+    usersRef.orderByKey().equalTo(qusername).once('value', (snapshot) => {
+        if (!snapshot.exists()) res.json({ result: false, ErrorMessage: "UserNotRegistered!" });
+        else {
+            if (snapshot.child(qusername).val().password == qpassword) res.json({ result: true });
+            else res.json({ result: false, ErrorMessage: "WrongPassword!" });
+        }
     })
 });
